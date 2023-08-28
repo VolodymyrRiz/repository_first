979f7d88db545b635dd6ebc176e4b942b77f3408
VolodymyrRiz <v.v.rizun@gmail.com>
2023-08-28 14:11:09 +0300

    pract9

--- money.py
+++ money.py
@@ -3,6 +3,7 @@ a = int(input("Enter coefficient a: "))
 b = int(input("Enter coefficient b: "))
 c = int(input("Enter coefficient c: "))
 D = b ** 2 - 4 * a * c
+print(D)
 if D > 0:
     x1 = (-b + math.sqrt(D)) / (2 * a)
     x2 = (-b - math.sqrt(D)) / (2 * a)
