a43df9bab56c68a393ac8c3f8b144dc8a2c99976
VolodymyrRiz <v.v.rizun@gmail.com>
2023-08-28 23:57:15 +0300

    mon4

--- money.py
+++ money.py
@@ -1,5 +1,9 @@
 num = int(input("Enter an integer number: "))
 num = num % 2
 print(num)
-is_even = "Odd" if num > 0 else "Even"
-print(is_even)
+if num == 0:
+    is_even = False
+else:
+    is_even = True
+is_even1 = is_even if num > 0 else False
+print(is_even1)
